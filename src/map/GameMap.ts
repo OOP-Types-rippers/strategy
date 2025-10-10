f7fc5d01d6891c9abe62eb7aa4623499df1225ca
savedheart <123maks45678@gmail.com>
2025-10-10 19:09:53 +0300

    game map first commit

---
+++ src/map/GameMap.ts
@@ -0,0 +1,36 @@
+import type { ITile } from "../types/ITile";
+import type { TerrainType } from "../types/TileTerrain"
+import {IGameMap} from "../types/IGameMap";
+import {Tile} from "./Tile";
+
+export class GameMap implements IGameMap {
+    width: number;
+    height: number;
+    grid: ITile[][];
+
+    constructor(width: number, height: number, defaultTerrain: TerrainType = 'grass') {
+        this.width = width;
+        this.height = height;
+        this.grid = [];
+
+        for (let y = 0; y < height; y++) {
+            const row: ITile[] = [];
+            for (let x = 0; x < width; x++) {
+                row.push(new Tile(defaultTerrain));
+            }
+            this.grid.push(row);
+        }
+    }
+
+    getTile(x: number, y: number): ITile | null {
+        if (x < 0 || y < 0 || x >= this.width || y >= this.height) return null;
+        return this.grid[y][x];
+    }
+
+    setTile(x: number, y: number, terrain: TerrainType): void {
+        const tile = this.getTile(x, y);
+        if (tile) {
+            tile.terrain = terrain;
+        }
+    }
+}
