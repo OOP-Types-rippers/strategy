7d2d5ec0a8d32a8cb9199a47a4b40b7b002371e4
sohodolly <119806383+sohodolly@users.noreply.github.com>
2026-01-24 15:49:55 +0200

    Update IBuilding.ts

--- src/types/IBuilding.ts
+++ src/types/IBuilding.ts
@@ -16,8 +16,4 @@ export interface IBuilding {
   isActive: boolean;
   activate(): void;
   info(): string;
-  getState(): IBuildingState;
-  restoreFromState(state: IBuildingState, context: IRestoreContext): void;
-  getUpgradeCost(): number;
-  canUpgrade(): boolean;
 }
