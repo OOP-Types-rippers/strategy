9d3b42a71f59564f4e597cfc3ae252804cdb4f1c
dmytro-mar <marchuk.dmytro1@lll.kpi.ua>
2026-05-23 20:39:41 +0300

    Add factions generation functionality (#62)

--- src/factions/Faction.ts
+++ src/factions/Faction.ts
@@ -15,3 +15,15 @@ export class Faction implements IFaction {
     }
   }
 }
+
+export function generateFactions(count: number, startingMoney: number = 100): Faction[] {
+  if (count < 1 || count > 8) {
+    throw new Error("Number of factions must be between 1 and 8");
+  }
+
+  const factions: Faction[] = [];
+  for (let i = 0; i < count; i++) {
+    factions.push(new Faction(FACTION_NAMES[i]!, FACTION_COLORS[i]!, startingMoney));
+  }
+  return factions;
+}
