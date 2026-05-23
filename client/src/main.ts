7ad660c7603a41681aa31db26536734e801adeb1
Dmytro Marchuk <marchuk.dmytro1@lll.kpi.ua>
2026-05-23 21:35:03 +0300

    feat: assign input to factions

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import { GameController } from "../../src/core/GameController";
 import { HTMLRenderer } from "./renderers/HTMLRenderer";
 import { MapGenerator } from "../../src/map/MapGenerator";
 import { FactionsGenerator } from "../../src/factions/FactionsGenerator";
+import { HTMLInputController } from "./io/HTMLInputController";
 
 function createGame(mapSize: number, factionCount: number) {
   const mapGenerator = new MapGenerator();
@@ -12,8 +13,13 @@ function createGame(mapSize: number, factionCount: number) {
   const factionsGenerator = new FactionsGenerator();
   const factions = factionsGenerator.generateFactions(factionCount);
 
+  
   const gameController = new GameController(map, factions, renderer);
-  // TODO: assign inputs
+
+  const inputController = new HTMLInputController(gameController, renderer);
+  for (const faction of factions) {
+    gameController.registerController(faction, inputController);
+  }
 
   return gameController;
 }
